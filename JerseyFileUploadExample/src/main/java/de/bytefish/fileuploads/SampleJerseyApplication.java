43a40086eb4d6dc7c30a64377d58ce7c63e4a291
Philipp Wagner <bytefish@gmx.de>
2017-10-08 13:24:44 +0200

    Copyright Header

--- JerseyFileUploadExample/src/main/java/de/bytefish/fileuploads/SampleJerseyApplication.java
+++ JerseyFileUploadExample/src/main/java/de/bytefish/fileuploads/SampleJerseyApplication.java
@@ -1,3 +1,6 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package de.bytefish.fileuploads;
 
 import de.bytefish.fileuploads.provider.IRootPathProvider;
@@ -6,8 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SampleJerseyApplication extends SpringBootServletInitializer {
